235860b53b0b6d1ef6ed6920bf517107e1ce86d5
Drawain <drawain@gmail.com>
2015-10-02 10:20:13 +0200

    GM L2

--- player.js
+++ player.js
@@ -4,14 +4,35 @@ module.exports = {
   VERSION: "GrandMaster Level 1",
 
   bet_request: function(gameState) {
-    console.log('Actual game state', JSON.stringify(game_state, null, 4));
+    console.log('Actual game state', JSON.stringify(gameState, null, 4));
 
     var player = gameState.players[gameState.in_action];
     var hand = player.hole_cards;
 
-    if (hand[0].rank == hand[1].rank) {
+    var getRankValue = function(card) {
+      switch (card.rank) {
+        case 'J': return 11;
+        case 'Q': return 12;
+        case 'K': return 13;
+        case 'A': return 14;
+        default: parseFloat(card.rank);
+      }
+    };
+
+
+    if (getRankValue(hand[0]) == getRankValue(hand[1])) {
+      return gameState.minimum_raise * 3;
+    }
+
+    if (Math.abs(getRankValue(hand[0]) - getRankValue(hand[1])) === 1) {
       return gameState.minimum_raise * 2;
     }
+
+
+    if (hand[0].suit == hand[1].suit) {
+      return gameState.minimum_raise;
+    }
+
 
     return 0;
   },
