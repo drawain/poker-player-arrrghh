e6831d4d26c36b8221e6799fd2218b716c2d4ebb
Drawain <drawain@gmail.com>
2015-10-02 10:22:43 +0200

    Saften GM

--- player.js
+++ player.js
@@ -1,38 +1,41 @@
 
 module.exports = {
 
-  VERSION: "GrandMaster Level 1",
+  VERSION: "GrandMaster Level 1.2 SafeWay",
 
   bet_request: function(gameState) {
-    console.log('Actual game state', JSON.stringify(gameState, null, 4));
+    try {
+      console.log('Actual game state', JSON.stringify(gameState, null, 4));
 
-    var player = gameState.players[gameState.in_action];
-    var hand = player.hole_cards;
+      var player = gameState.players[gameState.in_action];
+      var hand = player.hole_cards;
 
-    var getRankValue = function(card) {
-      switch (card.rank) {
-        case 'J': return 11;
-        case 'Q': return 12;
-        case 'K': return 13;
-        case 'A': return 14;
-        default: parseFloat(card.rank);
+      var getRankValue = function(card) {
+        switch (card.rank) {
+          case 'J': return 11;
+          case 'Q': return 12;
+          case 'K': return 13;
+          case 'A': return 14;
+          default: parseFloat(card.rank);
+        }
+      };
+
+
+      if (getRankValue(hand[0]) == getRankValue(hand[1])) {
+        return gameState.minimum_raise * 3;
       }
-    };
+
+      if (Math.abs(getRankValue(hand[0]) - getRankValue(hand[1])) === 1) {
+        return gameState.minimum_raise * 2;
+      }
 
 
-    if (getRankValue(hand[0]) == getRankValue(hand[1])) {
-      return gameState.minimum_raise * 3;
-    }
-
-    if (Math.abs(getRankValue(hand[0]) - getRankValue(hand[1])) === 1) {
-      return gameState.minimum_raise * 2;
-    }
+      if (hand[0].suit == hand[1].suit) {
+        return gameState.minimum_raise;
+      }
 
 
-    if (hand[0].suit == hand[1].suit) {
-      return gameState.minimum_raise;
-    }
-
+    } catch(e) {}
 
     return 0;
   },
