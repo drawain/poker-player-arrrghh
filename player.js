0f37a8651b10775571b19b6040163408fe90d6a4
Drawain <drawain@gmail.com>
2015-10-02 12:15:19 +0200

    GM L2.1 SupaDupa RefactorMaster

--- player.js
+++ player.js
@@ -1,7 +1,44 @@
+var Hand = function(hand) {
+  this._hand = hand;
+};
+
+Hand.prototype = {
+  getRankValue: function(card) {
+    switch (card.rank) {
+      case 'J':
+        return 11;
+      case 'Q':
+        return 12;
+      case 'K':
+        return 13;
+      case 'A':
+        return 14;
+      default:
+        return parseFloat(card.rank);
+    }
+  },
+
+  rankIsSame: function() {
+    return this.getRankValue(this._hand[0]) == this.getRankValue(this._hand[1]);
+  },
+
+  suiteIsSame: function() {
+    return this._hand[0].suit == this._hand[1].suit;
+  },
+
+  diffIsOne: function() {
+    return Math.abs(this.getRankValue(this._hand[0]) - this.getRankValue(this._hand[1])) === 1;
+  },
+
+  rankHasBiggerThan: function(rank) {
+    return (this.getRankValue(this._hand[0]) > rank) || (this.getRankValue(this._hand[1]) > rank);
+  }
+};
+
 
 module.exports = {
 
-  VERSION: "GrandMaster Level 2 SupaDupa",
+  VERSION: "GrandMaster Level 2 RefactorMaster",
 
   bet_request: function(gameState) {
     try {
@@ -10,59 +47,43 @@ module.exports = {
       var player = gameState.players[gameState.in_action];
       var hand = player.hole_cards;
 
-      var isPreFlop = function() {
+      var isPreFlop = function(gameState) {
         return gameState.community_cards.length === 0;
       };
 
-      var getRankValue = function(card) {
-        switch (card.rank) {
-          case 'J': return 11;
-          case 'Q': return 12;
-          case 'K': return 13;
-          case 'A': return 14;
-          default: parseFloat(card.rank);
-        }
-      };
 
-      var rankIsSame = function(hand) {
-        return getRankValue(hand[0]) == getRankValue(hand[1]);
-      };
-
-      var suiteIsSame = function(hand) {
-        return hand[0].suit == hand[1].suit;
-      };
-
-      var diffIsOne = function(hand) {
-        return Math.abs(getRankValue(hand[0]) - getRankValue(hand[1])) === 1;
-      };
-
-
-      if (isPreFlop()) {
-        if (rankIsSame(hand) && getRankValue(hand[0]) > 7) {
+      var getPreFlopBet = function(gameState, player, hand) {
+        if (hand.rankIsSame() && hand.rankHasBiggerThan(7)) {
           return player.stack;
         }
 
-        if (rankIsSame(hand)) {
+        if (hand.rankIsSame()) {
           return gameState.minimum_raise * 40;
         }
 
-        if (diffIsOne(hand) && suiteIsSame(hand)) {
+        if (hand.diffIsOne() && hand.suiteIsSame()) {
           return gameState.minimum_raise * 30;
         }
 
-        if (diffIsOne(hand)) {
+        if (hand.diffIsOne()) {
           return gameState.minimum_raise * 20;
         }
 
-        if (suiteIsSame(hand)) {
+        if (hand.suiteIsSame()) {
           return gameState.minimum_raise * 10;
         }
+      };
+
+
+      if (isPreFlop(gameState)) {
+        return getPreFlopBet(gameState, player, new Hand(hand));
       } else {
         return gameState.current_buy_in - player.bet;
       }
 
 
-    } catch(e) {}
+    } catch (e) {
+    }
 
     return 0;
   },
