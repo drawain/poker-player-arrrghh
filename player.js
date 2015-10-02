af94065e155b4bbddfafceb54dd1936c1adf4c8d
Drawain <drawain@gmail.com>
2015-10-02 10:04:14 +0200

    Echo game state for testing purposes

--- player.js
+++ player.js
@@ -4,6 +4,7 @@ module.exports = {
   VERSION: "Default JavaScript folding player",
 
   bet_request: function(game_state) {
+    console.log('Actual game state', JSON.stringify(game_state, null, 4));
     return 1000;
   },
 
